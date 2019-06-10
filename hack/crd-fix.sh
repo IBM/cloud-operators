1f2a33471eeba96a307ed35217ac2a4f792b00dd
Paolo Dettori <dettori@us.ibm.com>
2019-06-10 10:16:48 -0400

    fix CRD for functions

--- hack/crd-fix.sh
+++ hack/crd-fix.sh
@@ -2,13 +2,15 @@
 #
 # A script to fix CRD generations
 
+# Note: script depends on line numbers so if anything is changed in the CRD generation it needs to be adjusted.
+
 SCRIPTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 FILE_PATH=$SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_function.yaml
-sed -i .bak "130s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_function.yaml
-sed -i .bak "62s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_invocation.yaml
-sed -i .bak "100s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_invocation.yaml
-sed -i .bak "88s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_package.yaml
-sed -i .bak "88s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_trigger.yaml
+sed -i .bak "137s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_function.yaml
+sed -i .bak "69s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_invocation.yaml
+sed -i .bak "107s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_invocation.yaml
+sed -i .bak "95s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_package.yaml
+sed -i .bak "89s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_trigger.yaml
 
 # remove the .bak as they create issues with the releases
 rm $SCRIPTDIR/../config/crds/*.yaml.bak
