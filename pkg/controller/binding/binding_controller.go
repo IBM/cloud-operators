a4e197859ea6f318521e4100882e5b1b2aa286d9
Mandana Vaziri <mvaziri@us.ibm.com>
2019-06-11 11:35:20 -0400

    bug fix (#30)

--- pkg/controller/binding/binding_controller.go
+++ pkg/controller/binding/binding_controller.go
@@ -86,7 +86,7 @@ func newReconciler(mgr manager.Manager) reconcile.Reconciler {
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	// Create a new controller
-	c, err := controller.New("binding-controller", mgr, controller.Options{Reconciler: r, MaxConcurrentReconciles: 30})
+	c, err := controller.New("binding-controller", mgr, controller.Options{Reconciler: r, MaxConcurrentReconciles: 32})
 	if err != nil {
 		return err
 	}
