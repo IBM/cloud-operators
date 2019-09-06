c03159dda011cad2324c043aeaa5e41c95fbb023
mvaziri <mvaziri@us.ibm.com>
2019-09-06 15:10:13 -0400

    CRD fix

---
+++ hack/crd-fix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# A script to fix CRD generations
+
+# Note: script depends on line numbers so if anything is changed in the CRD generation it needs to be adjusted.
+
+SCRIPTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+sed -i.bak "73s/.*//" $SCRIPTDIR/../config/crds/ibmcloud_v1alpha1_service.yaml
+
+
+# remove the .bak as they create issues with the releases
+rm $SCRIPTDIR/../config/crds/*.yaml.bak
