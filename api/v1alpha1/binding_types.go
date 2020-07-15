0aa320a2d094370db96800da8deab16dd533a201
John Starich <jstarich@us.ibm.com>
2020-07-15 12:20:56 -0500

    initial kubebuilder build
    Signed-off-by: John Starich <johnstarich@johnstarich.com>

---
+++ api/v1alpha1/binding_types.go
@@ -0,0 +1,63 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// BindingSpec defines the desired state of Binding
+type BindingSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Binding. Edit Binding_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// BindingStatus defines the observed state of Binding
+type BindingStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+
+// Binding is the Schema for the bindings API
+type Binding struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   BindingSpec   `json:"spec,omitempty"`
+	Status BindingStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// BindingList contains a list of Binding
+type BindingList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Binding `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Binding{}, &BindingList{})
+}
