f28ce149b8aeb2e614c422acec01bf63da6f65b9
John Starich <johnstarich@johnstarich.com>
2020-08-24 13:13:01 -0500

    Fix wording
    Signed-off-by: John Starich <johnstarich@johnstarich.com>

--- hack/config-operator.sh
+++ hack/config-operator.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Back-compatible installer. Simply runs the new installer instead.
 
-echo 'WARNING: This installer is deprecated and may be removed in future version.' >&2
+echo 'WARNING: This installer is deprecated and may be removed in a future version.' >&2
 echo 'Please see here for the most up-to-date install script: https://github.com/ibm/cloud-operators' >&2
 curl -sL https://raw.githubusercontent.com/IBM/cloud-operators/master/hack/configure-operator.sh | bash
