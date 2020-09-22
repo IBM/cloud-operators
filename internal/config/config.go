cb323dd12daefeb65b167984274dc103e704f111
John Starich <johnstarich@johnstarich.com>
2020-09-21 21:36:58 -0500

    Add back max concurrent reconciles environment variable (#210)
    * MaxConcurrentReconciles can be set through env var
    * Combine max reconciles into main Config struct, surface it in deployment.yaml
    
    Co-authored-by: qibobo <qiyangbj@cn.ibm.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,20 +13,22 @@ var (
 )
 
 type Config struct {
-	APIKey              string        `envconfig:"bluemix_api_key"`
-	AccountID           string        `envconfig:"bluemix_account_id"`
-	ControllerNamespace string        `envconfig:"controller_namespace"`
-	Org                 string        `envconfig:"bluemix_org"`
-	Region              string        `envconfig:"bluemix_region"`
-	ResourceGroupName   string        `envconfig:"bluemix_resource_group"`
-	Space               string        `envconfig:"bluemix_space"`
-	SyncPeriod          time.Duration `envconfig:"sync_period"`
+	APIKey                  string        `envconfig:"bluemix_api_key"`
+	AccountID               string        `envconfig:"bluemix_account_id"`
+	ControllerNamespace     string        `envconfig:"controller_namespace"`
+	MaxConcurrentReconciles int           `envconfig:"max_concurrent_reconciles"`
+	Org                     string        `envconfig:"bluemix_org"`
+	Region                  string        `envconfig:"bluemix_region"`
+	ResourceGroupName       string        `envconfig:"bluemix_resource_group"`
+	Space                   string        `envconfig:"bluemix_space"`
+	SyncPeriod              time.Duration `envconfig:"sync_period"`
 }
 
 func Get() Config {
 	loadOnce.Do(func() {
 		config = Config{ // default values
-			SyncPeriod: 150 * time.Second,
+			MaxConcurrentReconciles: 1,
+			SyncPeriod:              150 * time.Second,
 		}
 		envconfig.MustProcess("", &config)
 	})
