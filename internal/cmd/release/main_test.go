74ffae534a0bca3922bddc2c7236d604ff5fc698
John Starich <johnstarich@johnstarich.com>
2021-08-31 20:30:35 -0500

    Test CRD updates
    Signed-off-by: John Starich <johnstarich@johnstarich.com>

--- internal/cmd/release/main_test.go
+++ internal/cmd/release/main_test.go
@@ -22,13 +22,13 @@ func TestRun(t *testing.T) {
 		someVersion   = "v1.2.3"
 		someUserName  = "Joe Schmoe"
 		someUserEmail = "joe@example.com"
-		someCRDGlob   = "apiextensions.k8s.io_*_customresourcedefinition_*.ibmcloud.ibm.com.yaml"
 	)
 	dir := t.TempDir()
 	someCSVFile := filepath.Join(dir, "ibmcloud-operator.package.yaml")
 	require.NoError(t, ioutil.WriteFile(someCSVFile, []byte(`CSV contents`), 0600))
 	somePackageFile := filepath.Join(dir, "ibmcloud_operator.v1.2.3.clusterserviceversion.yaml")
 	require.NoError(t, ioutil.WriteFile(somePackageFile, []byte(`package contents`), 0600))
+	someCRDGlob := filepath.Join(dir, "apiextensions.k8s.io_*_customresourcedefinition_*.ibmcloud.ibm.com.yaml")
 	someCRDFile := filepath.Join(dir, "apiextensions.k8s.io_v1beta1_customresourcedefinition_mycrd.ibmcloud.ibm.com.yaml")
 	require.NoError(t, ioutil.WriteFile(someCRDFile, []byte(`CRD contents`), 0600))
 
@@ -95,9 +95,11 @@ OpenShift PR opened: %s
 			}
 			var (
 				k8sMainSHA          = newSHA()
+				k8sCRDSHA           = newSHA()
 				k8sPackageSHA       = newSHA()
 				k8sCSVSHA           = newSHA()
 				openshiftMainSHA    = newSHA()
+				openshiftCRDSHA     = newSHA()
 				openshiftPackageSHA = newSHA()
 				openshiftCSVSHA     = newSHA()
 			)
@@ -109,24 +111,28 @@ OpenShift PR opened: %s
 			)
 			handler := newMockServerHandler(t, []mockRequest{
 				// k8s
-				{http.MethodGet, k8sRepo + "/git/ref/heads/main"},                                                                                 // get default branch from upstream
-				{http.MethodPatch, k8sForkRepo + "/git/refs/heads/main"},                                                                          // force update fork default branch to same commit
-				{http.MethodGet, k8sForkRepo + "/git/ref/heads/release-v1.2.3"},                                                                   // get release branch info
-				{http.MethodPatch, k8sForkRepo + "/git/refs/heads/release-v1.2.3"},                                                                // branch exists, force update to same commit
-				{http.MethodGet, k8sForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/ibmcloud_operator.v1.2.3.clusterserviceversion.yaml"}, // get old CSV hash
-				{http.MethodPut, k8sForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/ibmcloud_operator.v1.2.3.clusterserviceversion.yaml"}, // update CSV
-				{http.MethodGet, k8sForkRepo + "/contents/operators/ibmcloud-operator/ibmcloud-operator.package.yaml"},                            // get old package hash
-				{http.MethodPut, k8sForkRepo + "/contents/operators/ibmcloud-operator/ibmcloud-operator.package.yaml"},                            // update package
+				{http.MethodGet, k8sRepo + "/git/ref/heads/main"},                  // get default branch from upstream
+				{http.MethodPatch, k8sForkRepo + "/git/refs/heads/main"},           // force update fork default branch to same commit
+				{http.MethodGet, k8sForkRepo + "/git/ref/heads/release-v1.2.3"},    // get release branch info
+				{http.MethodPatch, k8sForkRepo + "/git/refs/heads/release-v1.2.3"}, // branch exists, force update to same commit
+				{http.MethodGet, k8sForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/apiextensions.k8s.io_v1beta1_customresourcedefinition_mycrd.ibmcloud.ibm.com.yaml"}, // get old CRD hash
+				{http.MethodPut, k8sForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/apiextensions.k8s.io_v1beta1_customresourcedefinition_mycrd.ibmcloud.ibm.com.yaml"}, // update CRD
+				{http.MethodGet, k8sForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/ibmcloud_operator.v1.2.3.clusterserviceversion.yaml"},                               // get old CSV hash
+				{http.MethodPut, k8sForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/ibmcloud_operator.v1.2.3.clusterserviceversion.yaml"},                               // update CSV
+				{http.MethodGet, k8sForkRepo + "/contents/operators/ibmcloud-operator/ibmcloud-operator.package.yaml"},                                                          // get old package hash
+				{http.MethodPut, k8sForkRepo + "/contents/operators/ibmcloud-operator/ibmcloud-operator.package.yaml"},                                                          // update package
 
 				// openshift
-				{http.MethodGet, ocRepo + "/git/ref/heads/main"},                                                                                 // get default branch from upstream
-				{http.MethodPatch, ocForkRepo + "/git/refs/heads/main"},                                                                          // force update fork default branch to same commit
-				{http.MethodGet, ocForkRepo + "/git/ref/heads/release-v1.2.3"},                                                                   // get release branch info
-				{http.MethodPost, ocForkRepo + "/git/refs"},                                                                                      // branch does not exist, create new one at same commit
-				{http.MethodGet, ocForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/ibmcloud_operator.v1.2.3.clusterserviceversion.yaml"}, // get old CSV hash
-				{http.MethodPut, ocForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/ibmcloud_operator.v1.2.3.clusterserviceversion.yaml"}, // update CSV
-				{http.MethodGet, ocForkRepo + "/contents/operators/ibmcloud-operator/ibmcloud-operator.package.yaml"},                            // get old package hash
-				{http.MethodPut, ocForkRepo + "/contents/operators/ibmcloud-operator/ibmcloud-operator.package.yaml"},                            // update package
+				{http.MethodGet, ocRepo + "/git/ref/heads/main"},               // get default branch from upstream
+				{http.MethodPatch, ocForkRepo + "/git/refs/heads/main"},        // force update fork default branch to same commit
+				{http.MethodGet, ocForkRepo + "/git/ref/heads/release-v1.2.3"}, // get release branch info
+				{http.MethodPost, ocForkRepo + "/git/refs"},                    // branch does not exist, create new one at same commit
+				{http.MethodGet, ocForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/apiextensions.k8s.io_v1beta1_customresourcedefinition_mycrd.ibmcloud.ibm.com.yaml"}, // get old CRD hash
+				{http.MethodPut, ocForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/apiextensions.k8s.io_v1beta1_customresourcedefinition_mycrd.ibmcloud.ibm.com.yaml"}, // update CRD
+				{http.MethodGet, ocForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/ibmcloud_operator.v1.2.3.clusterserviceversion.yaml"},                               // get old CSV hash
+				{http.MethodPut, ocForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/ibmcloud_operator.v1.2.3.clusterserviceversion.yaml"},                               // update CSV
+				{http.MethodGet, ocForkRepo + "/contents/operators/ibmcloud-operator/ibmcloud-operator.package.yaml"},                                                          // get old package hash
+				{http.MethodPut, ocForkRepo + "/contents/operators/ibmcloud-operator/ibmcloud-operator.package.yaml"},                                                          // update package
 
 				// pulls
 				{http.MethodGet, k8sRepo + "/pulls"}, // check existing k8s PRs (found one, don't open anew)
@@ -154,6 +160,12 @@ OpenShift PR opened: %s
 					}`
 				},
 				{http.MethodPatch, "/repos/some-fork/community-operators/git/refs/heads/release-v1.2.3"}: func(r *http.Request, body string) (int, string) {
+					return http.StatusOK, `{}`
+				},
+				{http.MethodGet, k8sForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/apiextensions.k8s.io_v1beta1_customresourcedefinition_mycrd.ibmcloud.ibm.com.yaml"}: func(r *http.Request, body string) (int, string) {
+					return http.StatusOK, fmt.Sprintf(`{"sha": %q}`, k8sCRDSHA)
+				},
+				{http.MethodPut, k8sForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/apiextensions.k8s.io_v1beta1_customresourcedefinition_mycrd.ibmcloud.ibm.com.yaml"}: func(r *http.Request, body string) (int, string) {
 					return http.StatusOK, `{}`
 				},
 				{http.MethodGet, "/repos/some-fork/community-operators/contents/operators/ibmcloud-operator/ibmcloud-operator.package.yaml"}: func(r *http.Request, body string) (int, string) {
@@ -188,6 +200,12 @@ OpenShift PR opened: %s
 				{http.MethodPost, "/repos/some-fork/community-operators-prod/git/refs"}: func(r *http.Request, body string) (int, string) {
 					assert.Contains(t, body, fmt.Sprintf(`"sha":%q`, openshiftMainSHA))
 					assert.Contains(t, body, `"ref":"refs/heads/release-v1.2.3"`)
+					return http.StatusOK, `{}`
+				},
+				{http.MethodGet, ocForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/apiextensions.k8s.io_v1beta1_customresourcedefinition_mycrd.ibmcloud.ibm.com.yaml"}: func(r *http.Request, body string) (int, string) {
+					return http.StatusOK, fmt.Sprintf(`{"sha": %q}`, openshiftCRDSHA)
+				},
+				{http.MethodPut, ocForkRepo + "/contents/operators/ibmcloud-operator/1.2.3/apiextensions.k8s.io_v1beta1_customresourcedefinition_mycrd.ibmcloud.ibm.com.yaml"}: func(r *http.Request, body string) (int, string) {
 					return http.StatusOK, `{}`
 				},
 				{http.MethodGet, "/repos/some-fork/community-operators-prod/contents/operators/ibmcloud-operator/ibmcloud-operator.package.yaml"}: func(r *http.Request, body string) (int, string) {
@@ -277,7 +295,7 @@ func newMockServerHandler(t *testing.T, expectRequests []mockRequest, responses 
 		t.Helper()
 		assert.Equal(t, len(expectRequests), m.currentRequest, "Number of handled requests must equal expected request count")
 
-		if !assert.Empty(t, m.unusedResponses, "Found responses that were not used. Fix or remove these:") {
+		if !t.Failed() && !assert.Empty(t, m.unusedResponses, "Found responses that were not used. Fix or remove these:") {
 			for req := range m.unusedResponses {
 				t.Logf("Unused: %#v", req)
 			}
